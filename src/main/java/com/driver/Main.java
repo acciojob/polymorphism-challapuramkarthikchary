0f1458a56d2dae248cd6d83f7c8ddf6c7c9fb62b
challapuramkarthikchary <challapuramkarthikchary@gmail.com>
2025-10-28 00:45:51 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
+
+
 public class Main {
+
+  class Product{
+      public int product(int x, int y) {
+        return x+y;
+      }
+
+    public int product(int x, int y, int z) {
+      return x+y+z;
+    }
+    public double product(double x, double y) {
+      return x+y;
+    }
+  }
+
+  Product p = new Product();
+  p.product(4,3);
+ p.product(4,3,5);
+p.product(4.0,3.0);
+
+
+    
 
 }
